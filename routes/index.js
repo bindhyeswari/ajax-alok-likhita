298f4ccd2d124a957395708480df9810227d193e
BP Mishra <tech@golivelabs.io>
2015-09-15 12:55:37 -0700

    muklter

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
 var express = require('express');
+var multer = require('multer');
+var upload = multer({
+    dest: 'uploads/'
+});
 var router = express.Router();
 
 /* GET home page. */
@@ -11,6 +15,11 @@ router.post('/contacts', function (req, res) {
     res.status(201).json( {
       message: 'IMPL_101'
     });
+});
+
+router.post('/files/upload', upload.array('likhita', 5), function (req, res) {
+    // store the files in a directory
+    console.log(req.files);
 });
 
 module.exports = router;
