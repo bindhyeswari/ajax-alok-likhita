8f2d4a3f78c3e1ea0fe6cce007b1b45acf4ac2f1
BP Mishra <tech@golivelabs.io>
2015-09-15 13:31:27 -0700

    last draft

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var upload = multer({
     dest: 'uploads/'
 });
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -20,6 +21,20 @@ router.post('/contacts', function (req, res) {
 router.post('/files/upload', upload.array('likhita', 5), function (req, res) {
     // store the files in a directory
     console.log(req.files);
+    res.status(201).json(req.files);
+});
+
+router.get('/files', function (req, res) {
+    res.status(200).json([
+        {
+            link: '/files/0d29c58558f9c26c07aeed7c852c6895'
+        }
+    ]);
+});
+
+router.get('/files/:id', function (req, res) {
+    var filepathname = path.join(__dirname, '../uploads/' + req.params.id);
+    res.sendFile(filepathname);
 });
 
 module.exports = router;
